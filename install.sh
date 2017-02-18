02e5d0e5a5292445481de5c6dd6837e0e59e893c
liadgi <liadginosar@gmail.com>
2017-02-18 16:50:28 +0200

    adding install script

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install g++
+sudo apt-get install automake autoconf libtool libjansson-dev libcurl4-openssl-dev
+
+echo "/usr/local/lib" | sudo tee -a /etc/ld.so.conf
+sudo ldconfig
+
+cd libecap-1.0.0/ && ./configure && make && sudo make install
+
+cd ../c-vtapi-master/ && autoreconf -fi && ./configure && make && sudo make install
+
+cd ../ecap_clamav_adapter-2.0.0/ && env LIBS=" -lcvtapi" ./configure && make && sudo make install
+
+cd ../squid-3.5.22/ && \
+./configure --prefix=/usr \
+--localstatedir=/var \
+--libexecdir=${prefix}/lib/squid \
+--datadir=${prefix}/share/squid \
+--sysconfdir=/etc/squid \
+--with-default-user=proxy \
+--with-logdir=/var/log/squid \
+--with-pidfile=/var/run/squid.pid \
+--enable-ecap && \
+make && sudo make install
+
+cd ..
+yes | sudo cp -rf squid.conf /etc/squid/squid.conf
+
+sudo touch /var/run/squid.pid /var/log/squid/cache.log /var/log/squid/access.log 
+sudo chmod a+w /var/run/squid.pid /var/log/squid/cache.log /var/log/squid/access.log 
